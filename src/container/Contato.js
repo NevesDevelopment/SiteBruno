df494c1deea3bfc28533b8314ead190ca0e7eb1a
NevesDevelopment <nevesdevelop@gmail.com>
2020-03-18 08:34:26 -0300

    Nova correçao texto de agradecimento

--- src/container/Contato.js
+++ src/container/Contato.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import {Alert} from 'react-bootstrap';
 //----------------------------------------Carrega Rotas--------------------------------------------//
 import NavBar from '../ui/NavBar';
+import ContatoHome from './ContatoHome';
 //-------------------------------------------------------------------------------------------------//
 export default class Gessos extends Component {
     render() {
@@ -10,15 +11,7 @@ export default class Gessos extends Component {
 //--------------------------------Apresenta elementos em tela--------------------------------------//
             <div style={{marginTop:'20%'}}>
                 <NavBar />
-                <Alert variant="light">
-                    <Alert.Heading>&nbsp;Fale Comigo!</Alert.Heading>
-                    <hr />
-                    <p><i className="fab fa-whatsapp"></i>&nbsp;(00) 0000-0000</p>
-                    <p><i className="fas fa-mobile-alt"></i>&nbsp;(00) 0000-0000</p>
-                    <p><i className="fas fa-envelope-square"></i>&nbsp;brunoPintura@gmail.com</p>
-                    <hr />
-                    <p className="mb-0">Desde já, agradeço o seu contato e preferência!</p>
-                </Alert>
+                <ContatoHome/>
             </div>
         );
     }
